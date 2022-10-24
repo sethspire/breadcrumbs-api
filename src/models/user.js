89c39db97dbe768c24e8a3dad4750871014a2a95
Seth Spire <sspire2@eagles.bridgewater.edu>
2022-10-24 11:44:07 -0400

    update formatting

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose') 
+const mongoose = require('mongoose')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -9,7 +10,12 @@ const userSchema = new Schema(
       unique: true,
       required: true,
       trim: true,
-      lowercase: true
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error('Email is invalid.')
+        }
+      }
     },
     password: {
       type: String,
@@ -25,6 +31,17 @@ const userSchema = new Schema(
     }
   })
 
-  const User = mongoose.model('User', userSchema);
+userSchema.methods.toJSON = function() {
+  const user = this
+  
+  const userObject = user.toObject()
+  
+  delete userObject.password
+  delete userObject.__v
+  
+  return userObject
+}
 
-  module.exports = User
+const User = mongoose.model('User', userSchema);
+
+module.exports = User
