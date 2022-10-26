915248cab551cfb64abbbf70f59f50b87ab7e664
Seth Spire <sspire2@eagles.bridgewater.edu>
2022-10-26 11:14:19 -0400

    allow connections

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const cors = require('cors');
 require('./db/mongoose')
 
 const userRouter = require('./routers/users')
 
 const app = express()
+
+app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.json())
 
 app.use(userRouter)
