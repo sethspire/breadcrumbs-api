5e78881752073c21503041e99c9b817b187b57e7
Seth Spire <sspire2@eagles.bridgewater.edu>
2022-10-26 01:46:02 -0400

    logout endpoint

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema(
@@ -28,7 +29,13 @@ const userSchema = new Schema(
       unique: true,
       required: true,
       trim: true
-    }
+    },
+    tokens: [{
+      token: {
+          type: String,
+          required: true
+      }
+    }]
   })
 
 userSchema.methods.toJSON = function() {
@@ -37,7 +44,9 @@ userSchema.methods.toJSON = function() {
   const userObject = user.toObject()
   
   delete userObject.password
+  delete userObject._id
   delete userObject.__v
+  delete userObject.tokens
   
   return userObject
 }
@@ -49,9 +58,20 @@ userSchema.pre('save', async function(next) {
   if (user.isModified('password')) {
     user.password = await bcrypt.hash(user.password, 8)
   }
-  
+
   next()  // run the save() method
 })
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this
+ 
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JSON_WEB_TOKEN_SECRET)
+
+  user.tokens = user.tokens.concat({ token })
+  await user.save()
+
+  return token
+}
 
 const User = mongoose.model('User', userSchema);
 
--- src/routers/users.js
+++ src/routers/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
 const { sendWelcomeEmail } = require('../emails/account.js')
+const auth = require('../middleware/auth')
 
 const router = new express.Router()
 
@@ -10,11 +11,27 @@ router.post('/users', async (req, res) => {
 
   try {
     await user.save()
-    sendWelcomeEmail(user.email, user.name)
-    res.status(201).send(user)
+    const token = await user.generateAuthToken()
+
+    //sendWelcomeEmail(user.email, user.name)
+    res.status(201).send({user, token})
   } 
   catch(error) {
     res.status(400).send(error)
+  }
+})
+
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token
+    })
+    await req.user.save()
+    
+    res.send()
+  }
+  catch (e) {
+    res.status(500).send()
   }
 })
 
